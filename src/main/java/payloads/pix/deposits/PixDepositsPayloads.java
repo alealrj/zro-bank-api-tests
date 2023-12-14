f061f3492dc21397fda4228bd2f016107e26ac2a
ALEXANDRE ALVES RAMOS JUNIOR <60003723@midway.com.br>
2023-12-14 14:51:34 -0300

    Criando Payload do Pix Deposits

--- src/main/java/payloads/pix/deposits/PixDepositsPayloads.java
+++ src/main/java/payloads/pix/deposits/PixDepositsPayloads.java
@@ -1,6 +1,23 @@
 package payloads.pix.deposits;
 
+import factory.pix.deposits.PixDepositsQrCodesRequestFactory;
+import utils.FileOperations;
+
 public class PixDepositsPayloads {
 
-    
+    //Inicialização de serviços e utilidades
+    private final PixDepositsQrCodesRequestFactory pixDepositsQrCodesRequestFactory = new PixDepositsQrCodesRequestFactory();
+    private final FileOperations fileOperations = new FileOperations();
+
+    //Endpoints da API
+    private static final String POST_PIX_DEPOSITS_QRCODES_DYNAMIC_INSTANT_BILLING = "/pix/deposits/qr-codes/dynamic/instant-billing";
+    private static final String POST_PIX_DEPOSITS_QRCODES_DYNAMIC_DUE_DATE = "/pix/deposits/qr-codes/dynamic/due-date";
+    private static final String POST_PIX_DEPOSITS_QRCODES = "/pix/deposits/qr-codes";
+    private static final String GET_PIX_DEPOSITS_QRCODES_DYNAMIC_ID = "/pix/deposits/qr-codes/dynamic/{id}";
+    private static final String GET_V2_PIX_DEPOSITS_BYOPERATION_ID = "/v2/pix/deposits/by-operation/{id}";
+    private static final String GET_PIX_DEPOSITS_QRCODES_ID = "/pix/deposits/qr-codes/{id}";
+    private static final String GET_PIX_DEPOSITS_QRCODES = "/pix/deposits/qr-codes";
+    private static final String DEL_PIX_DEPOSITS_QRCODES_ID = "/pix/deposits/qr-codes/{id}";
+
+
 }
